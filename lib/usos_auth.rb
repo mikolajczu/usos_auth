e4cd9cd25e96fcce76062063dc6f14e3a11461bf
mikolajczu <mikeyczu@gmail.com>
2023-11-26 17:35:13 +0100

    init authenticator

--- lib/usos_auth.rb
+++ lib/usos_auth.rb
@@ -1,8 +1,38 @@
 # frozen_string_literal: true
 
-require_relative "usos_auth/version"
+require_relative 'usos_auth/version'
 
 module UsosAuth
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Authenticator
+    def initialize
+      # config = Configuration.load_config
+      consumer_key = 'temporary' # config['consumer_key']
+      consumer_secret = 'temporary' # config['consumer_secret']
+      site = 'temporary' # config['fields']
+      request_token_path = 'temporary' # config['fields']
+      authorize_url = 'temporary' # config['fields']
+      @access_token_path = 'temporary' # config['fields']
+      @callback_url = 'temporary' # config['callback_url']
+      @scopes = 'temporary' # config['scopes']
+      @fields = 'temporary' # config['fields']
+
+      @consumer = OAuth::Consumer.new(
+        consumer_key,
+        consumer_secret,
+        site: site,
+        request_token_path: request_token_path,
+        authorize_url: authorize_url,
+        access_token_path: callback_url
+      )
+    end
+
+    def get_request_token
+      @request_token = @consumer.get_request_token(
+        { oauth_callback: @callback_url },
+        { scope: @scopes }
+      )
+    end
+  end
 end
