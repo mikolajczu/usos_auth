b1bfdb8821c4c09f146bd2629f155ebdd884e69e
mikolajczu <mikeyczu@gmail.com>
2023-11-26 22:47:38 +0100

    add basic authentication logic

--- lib/usos_auth.rb
+++ lib/usos_auth.rb
@@ -1,38 +1,107 @@
 # frozen_string_literal: true
 
 require_relative 'usos_auth/version'
+require 'oauth'
 
 module UsosAuth
   class Error < StandardError; end
 
-  class Authenticator
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+
+  class Configuration
+    attr_accessor :consumer_key, :consumer_secret, :site, :request_token_path,
+                  :authorize_path, :access_token_path, :callback_url, :scopes, :fields
+
     def initialize
-      # config = Configuration.load_config
-      consumer_key = 'temporary' # config['consumer_key']
-      consumer_secret = 'temporary' # config['consumer_secret']
-      site = 'temporary' # config['fields']
-      request_token_path = 'temporary' # config['fields']
-      authorize_url = 'temporary' # config['fields']
-      @access_token_path = 'temporary' # config['fields']
-      @callback_url = 'temporary' # config['callback_url']
-      @scopes = 'temporary' # config['scopes']
-      @fields = 'temporary' # config['fields']
+      @consumer_key = ''
+      @consumer_secret = ''
+      @site = ''
+      @request_token_path = ''
+      @authorize_path = ''
+      @access_token_path = ''
+      @callback_url = ''
+      @scopes = ''
+      @fields = ''
+    end
+  end
+
+  class Authenticator
+    def authenticate
+      load_config
+      request_token
+      store_tokens(@consumer, @request_token, @site)
+      yield(authorize_url)
+    end
+
+    def callback(verifier)
+      load_stored_tokens
+      clear_stored_tokens
+      access_token = @request_token.get_access_token(oauth_verifier: verifier)
+
+      response = access_token.get('/services/users/user')
+
+      JSON.parse(response.body)
+    end
+
+    private
+
+    def load_config
+      configuration = UsosAuth.configuration
+      consumer_key = configuration.consumer_key
+      consumer_secret = configuration.consumer_secret
+      request_token_path = configuration.request_token_path
+      authorize_path = configuration.authorize_path
+      @site = configuration.site
+      @access_token_path = configuration.access_token_path
+      @callback_url = configuration.callback_url
+      @scopes = configuration.scopes
+      @fields = configuration.fields
 
       @consumer = OAuth::Consumer.new(
         consumer_key,
         consumer_secret,
-        site: site,
+        site: @site,
         request_token_path: request_token_path,
-        authorize_url: authorize_url,
-        access_token_path: callback_url
+        authorize_path: authorize_path,
+        access_token_path: @access_token_path
       )
     end
 
-    def get_request_token
+    def request_token
       @request_token = @consumer.get_request_token(
         { oauth_callback: @callback_url },
         { scope: @scopes }
       )
+    end
+
+    def authorize_url
+      @request_token.authorize_url
+    end
+
+    def store_tokens(consumer, request_token, site)
+      $stored_tokens ||= {}
+      $stored_tokens[:consumer] = consumer
+      $stored_tokens[:request_token] = request_token
+      $stored_tokens[:site] = site
+    end
+
+    def load_stored_tokens
+      @consumer = $stored_tokens[:consumer]
+      @request_token = $stored_tokens[:request_token]
+      @site = $stored_tokens[:site]
+    end
+
+    def clear_stored_tokens
+      $stored_tokens[:consumer] = nil
+      $stored_tokens[:request_token] = nil
+      $stored_tokens[:site] = nil
     end
   end
 end
--- usos_auth.gemspec
+++ usos_auth.gemspec
@@ -30,6 +30,7 @@ Gem::Specification.new do |spec|
 
   # Uncomment to register a new dependency of your gem
   spec.add_dependency "oauth", "~> 0.5.1"
+  gem 'faraday', '~> 2.3'
 
   # For more information and examples about making a new gem, check out our
   # guide at: https://bundler.io/guides/creating_gem.html
